ea2bf1464e7aed2f3390d88f0ae1844745d0dc29
thiago-santanna <uzmaki_thiago@outlook.com>
2022-05-26 20:42:37 -0300

    instalação do tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <h1 className="text-3xl font-bold">Lap Gerencial app</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
